a1536e27398d0d94e0d0f7c381c745fc06e70573
kodamanbou <kodamanbou0424@gmail.com>
2019-09-08 20:56:10 +0900

    Kuzushiji Recognition Network using YOLOv3.

--- yolov3.py
+++ yolov3.py
@@ -538,9 +538,10 @@ if __name__ == '__main__':
                     print('Epoch: {} \tloss: total: {} \txy: {} \twh: {} \tconf: {} \tclass{}'
                           .format(_global_step, _loss[0], _loss[1], _loss[2], _loss[3], _loss[4]))
 
-                    test_img = Image.open(input_dir + 'test_images/test_0a9b81ce.jpg')
+                    test_img = Image.open(input_dir + 'test_images/test_0a9b81ce.jpg').resize((416, 416), Image.LANCZOS)
                     test_h, test_w = test_img.height, test_img.width
-                    test_img = np.asarray([test_img.resize((416, 416))])
+                    test_img = np.asarray(test_img, np.float32)
+                    test_img = np.expand_dims(test_img, axis=0) / 255.
 
                     pred_boxes, pred_confs, pred_probs = yolo_model.predict(val_feature_maps)
                     pred_scores = pred_confs * pred_probs
